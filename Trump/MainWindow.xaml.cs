7f4385c11159a1da84712bff75e2aad064f44506
misskabu <kenta10000@gmail.com>
2019-05-01 21:48:18 +0900

    表の図柄を数字とスーツを指定して選べるようになった。

--- Trump/MainWindow.xaml.cs
+++ Trump/MainWindow.xaml.cs
@@ -33,12 +33,40 @@ namespace Trump
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var deck = new Deck();
-            Debug.Print("Deckのコンストラクタが起動");
-            Card card = deck.Cards[0];
+  
+            int number;
+            Suit suit;
+            
+            Enum.TryParse(ComboBox_Suit.Text,out suit);
+            if (suit.Equals(Suit.jocker))
+            {
+                number = 0;
+            }
+            else
+            {
+                number = Int32.Parse(ComboBox_Number.Text);
+            }
+            Card card = deck.Cards.Find(m => m.Number == number && m.Suit.Equals(suit));
             Debug.Print("このカードは:"+card.Suit.ToString() + card.Number);
             IntPtr hbitmap = card.ImageBitmap.GetHbitmap();
             Image1.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             DeleteObject(hbitmap);
+        }
+
+        private void ComboBox_Number_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < 14; i++)
+            {
+                ComboBox_Number.Items.Add(i.ToString());
+            }
+        }
+
+        private void ComboBox_Suit_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                ComboBox_Suit.Items.Add(suit.ToString());
+            }
         }
     }
 }
